29f30b43b6b1f8ee8349e1769327ad283e494d41
Simon Gu <simon@trymason.com>
2018-07-19 01:27:37 -0700

    added files

---
+++ src/components/Whyslack/index.js
@@ -0,0 +1,25 @@
+import { Component } from 'react';
+import h from 'react-hyperscript';
+import Mason from 'mason-library';
+
+import Whyslack from './Whyslack';
+
+export default class _Whyslack extends Component {
+  componentDidMount() {
+    Mason({
+      apiKey: "lF468QYy/S+M2LpKKIfxGsRqC18xARiJtoLXCAJ2Mhw=",
+      componentIds: [
+        "5b4674dc8c528d0003242f01", // Navbar
+        "5b4d175555ad93000368cab3", // Hero
+        "5b4e3ed2cfef9400036259ad", // CTA
+        "5b46aa0c8c528d00032431b4", // Footer
+        "5b477214ba8e5f0003457f7c", // Footer bottom
+        "5b4f8dd283d8f80003d6ef0d", // Body
+        "5b4f7631c36cae0003c78809", // clickable Page
+      ],
+    });
+  }
+  render() {
+    return h(Whyslack)
+  }
+}
