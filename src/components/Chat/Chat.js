f82432a5445496be2171a38316fbd57ae74a5538
tom <tim5046@gmail.com>
2018-07-18 18:36:59 -0700

    more chat chrome

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -10,7 +10,15 @@ export default class Chat extends Component {
           id: "5b4e968ecfef9400036264c0" // Left sidebar
         })
       ]),
-      h('.fixed.top-0.bg-white.w-100.overflow-scroll', { style: { minWidth: 400, bottom: 60, left: 219 }}, [
+      h('.absolute.top-0.right-0.bb.b--moon-gray.z-1', { style: { left: 219, width: 'calc(vw - 219px)'}}, [
+        h('.w-100', [
+          h(Canvas, {
+            id: '5b4fcdd583d8f80003d6f328',
+            children: h('.f6.b.dark-gray.tl.pt2', { style: { fontFamily: 'Proxima' }}, ['#channel'])
+          }) //Chat header
+        ]),
+      ]),
+      h('.fixed.bg-white.w-100.overflow-scroll', { style: { minWidth: 400, bottom: 60, left: 219, top: 58 }}, [
         h('.h-100.w-100', [
           h(Feed, {
             id: "5b4d13c355ad93000368ca6d", // Chat
--- src/components/Chat/index.js
+++ src/components/Chat/index.js
@@ -15,6 +15,7 @@ class _Chat extends Component {
         "5b4e968ecfef9400036264c0", // Left sidebar channel feed
         "5b4d13c355ad93000368ca6d", // Chat feed
         "5b4d5a8a55ad93000368cec5", // Create new message
+        "5b4fcdd583d8f80003d6f328", // Chat header
       ],
     });
   }
